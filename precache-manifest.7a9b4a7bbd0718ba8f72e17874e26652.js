e203a50488598789d93f22f735f6b893744e47eb
Garves <garves44@gmail.com>
2020-08-25 14:25:11 -0600

    Updates

---
+++ precache-manifest.7a9b4a7bbd0718ba8f72e17874e26652.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d452d452c1e8353d24dad98f274628a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c46edba3a8f056c5c91",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "f2d138882686de39fbe8",
+    "url": "/static/css/main.bc4a177c.chunk.css"
+  },
+  {
+    "revision": "3c46edba3a8f056c5c91",
+    "url": "/static/js/2.701bd7fd.chunk.js"
+  },
+  {
+    "revision": "effe4e92e8658ad9dfbf53e804a021fb",
+    "url": "/static/js/2.701bd7fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d138882686de39fbe8",
+    "url": "/static/js/main.3dc49a04.chunk.js"
+  },
+  {
+    "revision": "b1a250d4b61373345e22",
+    "url": "/static/js/runtime-main.210e1bba.js"
+  }
+]);
