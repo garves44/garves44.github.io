9526b7e0cfe6ece968de179a1889d587bd8a29c7
Garves <garves44@gmail.com>
2020-08-25 12:58:33 -0600

    updated all components and files

--- src/App.js
+++ src/App.js
@@ -1,50 +1,37 @@
-import React, { Component } from "react";
-import "./App.css";
-import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
-import Main from "./components/main";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
-        <Layout>
-          <Header
-            className="header-color"
-            title={
-              <Link style={{ textDecoration: "none", color: "white" }} to="/">
-                Jeremy Collins
-              </Link>
-            }
-            scroll
-          >
+    <Layout>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
+            <Navigation>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
             <Navigation>
               <Link to="/resume">Resume</Link>
               <Link to="/aboutme">About Me</Link>
               <Link to="/projects">Projects</Link>
               <Link to="/contact">Contact</Link>
             </Navigation>
-          </Header>
-          <Drawer
-            title={
-              <Link style={{ textDecoration: "none", color: "black" }} to="/">
-                MyPortfolio
-              </Link>
-            }
-          >
-            <Navigation>
-              <Link to="/resume">Resume</Link>
-              <Link to="/aboutme">About Me</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/contact">Contact</Link>
-            </Navigation>
-          </Drawer>
-          <Content>
+        </Drawer>
+        <Content>
             <div className="page-content" />
-            <Main />
-          </Content>
-        </Layout>
-      </div>
+            <Main/>
+        </Content>
+    </Layout>
+</div>
+
     );
   }
 }
