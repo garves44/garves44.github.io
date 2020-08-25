2572ffef6c7426d7ca1bf26705d350e0320063ba
Garves <garves44@gmail.com>
2020-08-25 16:45:24 -0600

    added shading and border to Main picture

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -14,10 +14,11 @@ class Landing extends Component {
                 borderRadius: "50%",
                 overflow: "visible",
                 boxShadow:
-                  "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+                  "0 8px 24px 0 rgba(0, 0, 0, 0.5), 0 8px 24px 0 rgba(0, 0, 0, 0.5)",
                 height: "300px",
                 width: "300px",
                 marginTop: "100px",
+                border: "solid 3px black",
               }}
             />
 
