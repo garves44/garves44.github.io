35a8ba3c35ee6d0815a68e21f08ba00e50d1f3fd
Garves <garves44@gmail.com>
2020-09-09 19:17:08 -0600

    changed button back to react-mdl button

--- src/components/projects.js
+++ src/components/projects.js
@@ -76,12 +76,9 @@ class Projects extends Component {
                 GitHub
               </Button>
               <Button colored>CodePen</Button>
-              <button onClick="window.location.href = 'https://garves44.github.io/p1-death-roll/'">
+              <Button colored href="https://garves44.github.io/p1-death-roll/">
                 Live Demo
-              </button>
-              {/* <Button colored href="https://garves44.github.io/p1-death-roll/">
-                Live Demo
-              </Button> */}
+              </Button>
             </CardActions>
             <CardMenu style={{ color: "#fff" }}>
               <IconButton name="share" />
